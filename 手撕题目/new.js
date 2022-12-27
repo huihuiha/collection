031fca9b5f9c85b4485bb45141381fcf91741c41
linguanghui <1476465526@qq.com>
2022-12-27 15:44:20 +0800

    add: new

--- 手撕题目/new.js
+++ 手撕题目/new.js
@@ -1,17 +1,20 @@
-
 function Fn(name) {
-    this.name = name
-    this.age = 20
-    return {1111:111}
+  this.name = name;
+  this.age = 20;
+  return { 1111: 111 };
 }
 Fn.prototype.say = function () {
-    console.log(this.age)
-}
+  console.log(this.age);
+};
 
 function mynew(Fn, ...args) {
-    const obj = {}
-    obj.__proto__ = Fn.prototype
-    let result = Fn.apply(obj, args)
-    return result instanceof Object ? result : obj
+  const obj = {};
+  obj.__proto__ = Fn.prototype;
+  let result = Fn.apply(obj, args);
+  // 1.如果 result 返回的是一个复杂类型，则返回 result
+  return result instanceof Object ? result : obj;
 }
 
+let obj = new Fn('qwe');
+console.log(myNew(Fn, 'qwe'));
+console.log(obj);
